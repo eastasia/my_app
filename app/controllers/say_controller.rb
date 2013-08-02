8cf08462737bb98c9541e7eefe7e8bd22aaf18db
Allan De Villa <ea_ehd021279IT@yahoo.com>
2013-08-03 07:40:02 +0800

    Added a Micropost resource

---
+++ app/controllers/say_controller.rb
@@ -0,0 +1,12 @@
+
+class SayController < ApplicationController
+  
+ 
+  def index
+    # @users = User.all
+  end
+  
+  def show
+  end 
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyApp::Application.config.secret_key_base = 'a0302024cfe7c1e343dcdd78ec860e01a925e4444003dd0477298d2bbdb5fa2b219a8e52350f452eb3206898c44e7421e54f6fcef907ed28dbc1f63124e979a6'
